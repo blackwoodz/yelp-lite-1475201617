b4c74a23814fd5fb21feabb05c159e69ab0d7022
Raghu Betina <raghu@firstdraft.com>
2016-09-30 02:16:17 +0000

    Direct association between ownerships and owner_responses

--- app/models/owner_response.rb
+++ app/models/owner_response.rb
@@ -1,6 +1,10 @@
 class OwnerResponse < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :class_name => "Ownership",
+             :foreign_key => "ownership_id"
+
   belongs_to :review
 
   # Indirect associations
