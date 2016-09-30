603306767bcfe99e9b1e518b9eb097ba1b278bd5
Raghu Betina <raghu@firstdraft.com>
2016-09-30 02:16:17 +0000

    Direct association between restaurants and taggings

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :tags,
+             :class_name => "Tagging",
+             :dependent => :destroy
+
   has_many   :reviews,
              :dependent => :destroy
 
